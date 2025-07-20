eee45dd44c90e8be4d49e617dd36c3b70b595c7d
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-07-20 21:23:14 +0300

    Delete alx-project-0x00-setup/alx-project-0x00-setup-main directory

--- alx-project-0x00-setup/alx-project-0x00-setup-main/alx-project-0x00/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-export interface PillProps {
-    title: string
-  }
-
-  export interface ButtonProps {
-    title: string; // Text to display on the button
-    styles?: string; // Additional CSS classes or styles
-  }
